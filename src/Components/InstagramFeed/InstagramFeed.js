cff7050cbac254226697703a1c68ab021fb5b663
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-01 20:48:30 +0900

    created components ready for editing

---
+++ src/Components/InstagramFeed/InstagramFeed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function InstagramFeed() {
+    return(
+        <div>
+            <h1>Instagram Feed</h1>
+        </div>
+    )
+}
+
+export default InstagramFeed
---
+++ src/Components/MenuTitle/MenuTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MenuTitle() {
+    return(
+        <div>
+            <h1>Menu Title</h1>
+        </div>
+    )
+}
+
+export default MenuTitle
