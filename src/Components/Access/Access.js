10b8357012105e7ac1484ce050ae749ee83c02ac
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-08 18:46:24 +0900

    Access section finished

--- src/Components/Access/Access.js
+++ src/Components/Access/Access.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Access.module.css'
 import { HiLocationMarker } from 'react-icons/hi'
 import { MdTrain } from 'react-icons/md'
+import { RiPhoneFill } from 'react-icons/ri'
 
 function Access() {
     return(
@@ -13,12 +14,25 @@ function Access() {
                     <h3>Location</h3>
                     
                     <div className={styles.mapContainer}>
-                        <iframe className={styles.map}src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.0008018847548!2d135.43078111523104!3d34.654682680446186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e88afbf2901b%3A0x69f4b578889abcd6!2s9%20Borden%20Coffee!5e0!3m2!1sen!2sjp!4v1622963201144!5m2!1sen!2sjp" style={{border: '0'}} allowFullScreen="" loading="lazy"></iframe>
+                        <iframe title="9 Borden Coffee Location" className={styles.map}src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.0008018847548!2d135.43078111523104!3d34.654682680446186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6000e88afbf2901b%3A0x69f4b578889abcd6!2s9%20Borden%20Coffee!5e0!3m2!1sen!2sjp!4v1622963201144!5m2!1sen!2sjp" style={{border: '0'}} allowFullScreen="" loading="lazy"></iframe>
                     </div>
-            
-                    <p>3 Chome-7-15 Chikko, Minato Ward, Osaka, 552-0021</p>
-                    <p>2 minute walk from Osakako Station (Chuo Line)</p>
-                    <p>080-1234-5678</p>
+
+                        
+                    <div className={styles.locationDetails}>
+                        <HiLocationMarker className={styles.icon}/>
+                        <p>3 Chome-7-15 Chikko, Minato Ward, Osaka, 552-0021</p>
+                    </div>
+                        
+                    <div className={styles.locationDetails}>
+                        <MdTrain className={styles.icon}/>
+                        <p>2 minute walk from Osakako Station (Chuo Line)</p>
+                    </div>
+                        
+                    <div className={styles.locationDetails}>
+                        <RiPhoneFill className={styles.icon}/>
+                        <p>080-1234-5678</p>
+                    </div>
+                    
                 </div>
 
                 <div className={styles.openingTimesArea}>
--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Carousel() {
     return(
         <div>
