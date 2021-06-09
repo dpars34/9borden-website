acc4bb1f0122f5a7b91e90ecd29b97acc598af16
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-09 16:50:43 +0900

    Carousel 1

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Carousel.module.css'
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
 
 
 function Carousel() {
+
+    const [ currentImage, setCurrentImage ] = useState(0)
+    const imageStyles = [styles.image1, styles.image2, styles.image3]
+
+    const nextImage = () => {
+        if (currentImage === (imageStyles.length - 1)) {
+            setCurrentImage(0)
+        }
+        else {
+            setCurrentImage(currentImage + 1)
+        }
+
+        console.log(currentImage)
+    }    
+
     return(
         <div>
-            <h1>Carousel</h1>
+            <div className={styles.carouselContainer} >
+                <div className={imageStyles[currentImage]}>
+                    <AiOutlineLeft className={styles.arrowLeft} />
+                    <AiOutlineRight className={styles.arrowRight} onClick={nextImage}/>
+                </div>
+            </div>
+            
+            
         </div>
     )   
 }
