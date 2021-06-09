fef1205ac7da2d6bdffd2d7f6d5efb510d2e4d9f
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-09 17:25:13 +0900

    carousel 2

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Carousel.module.css'
+import carouselData from './carouselData'
 
 
 function Carousel() {
+
+    const [ currentImage, setCurrentImage ] = useState(0)
+
     return(
-        <div>
-            <h1>Carousel</h1>
+        <div className={styles.carouselArea}>
+            {carouselData.map((image, index) => {
+                if (index === currentImage) {
+                    return (
+                        <img className={styles.image} src={image.imageSrc} alt="shop"/>
+                    )
+                }
+            })}
         </div>
     )   
 }
---
+++ src/Components/Carousel/carouselData.js
@@ -0,0 +1,19 @@
+import image1 from './carousel_images/IMG_7556.png'
+import image2 from './carousel_images/IMG_7619.png'
+import image3 from './carousel_images/IMG_7629.jpg'
+
+const carouselData = [
+    {
+        imageSrc: image1
+    },
+
+    {
+        imageSrc: image2
+    },
+
+    {
+        imageSrc: image3
+    }
+]
+
+export default carouselData
