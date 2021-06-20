9241f363a9c856767b11a19fc44d156159b15296
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-20 19:11:19 +0900

    adjust image sizes

--- src/Components/InstagramFeed/InstagramFeed.js
+++ src/Components/InstagramFeed/InstagramFeed.js
@@ -8,7 +8,7 @@ function InstagramFeed() {
         <div className={`instagram-feed ${styles.instagramArea}` }>
             <div className={styles.navDiv} id="instagram-feed"><MdWifi className={styles.icon}/><BsPlug className={styles.icon}/></div>
             <h1 className={styles.instagramFeedTitle}>Instagram Feed</h1>
-            <div id="curator-feed-default-feed-layout" className={styles.instagramFeed}><a href="https://www.instagram.com/9bordencoffee/?hl=en" rel="noreferrer" target="_blank" className="crt-logo crt-tag">Powered by Curator.io</a></div>
+            <div id="curator-feed-default-feed-layout" className={styles.instagramFeed}><a href="https://curator.io" rel="noreferrer" target="_blank" className="crt-logo crt-tag">Powered by Curator.io</a></div>
         </div>
     )
 }
