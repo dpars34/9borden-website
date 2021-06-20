625e47638de6822e99a3be6e4c09b3aba923c488
Daniel Parsons <dpars@hotmail.co.uk>
2021-06-20 18:53:22 +0900

    Preload images

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import styles from './Carousel.module.css'
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
+import image1 from './carousel_images/IMG_7619.jpg'
+import image2 from './carousel_images/IMG_7629.jpg'
+import image3 from './carousel_images/IMG_8435.jpg'
+import image4 from './carousel_images/IMG_7556.jpg'
 
 
 function Carousel() {
@@ -37,6 +41,28 @@ function Carousel() {
             }, 1)
         }
     }
+
+    function preloadImages(array) {
+        if (!preloadImages.list) {
+            preloadImages.list = [];
+        }
+        var list = preloadImages.list;
+        for (var i = 0; i < array.length; i++) {
+            var img = new Image();
+            img.onload = function() {
+                var index = list.indexOf(this);
+                if (index !== -1) {
+                    // remove image from the array once it's loaded
+                    // for memory consumption reasons
+                    list.splice(index, 1);
+                }
+            }
+            list.push(img);
+            img.src = array[i];
+        }
+    }
+    
+    preloadImages([image1, image2, image3, image4]);
 
     return(
         <div>
