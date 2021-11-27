736ee8da5d2513e1f3cc0bf899b740feca0bdd67
Daniel Parsons <dpars@hotmail.co.uk>
2021-11-27 12:04:41 +0900

    css edit

--- src/Text/newsText.js
+++ src/Text/newsText.js
@@ -25,3 +25,37 @@ export const japaneseText = [
         date: "2021年　11月"
     }
 ]
+
+/*
+
+参考として使ってください。
+
+export const englishText = [
+    {
+        title: "Number One",
+        info: "This is article one",
+        date: "Nov 2021"
+    },
+
+    {
+        title: "Number Two",
+        info: "This is article two",
+        date: "Nov 2021"
+    }
+]
+
+export const japaneseText = [
+    {
+        title: "一番",
+        info: "これは一番です",
+        date: "2021年 11月"
+    },
+
+    {
+        title: "二番",
+        info: "これは二番です",
+        date: "2021年　11月"
+    }
+]
+
+*/
