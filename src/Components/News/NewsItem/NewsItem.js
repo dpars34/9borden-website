0da779088622ffdaaa21fd50918399bd725edeb6
Daniel Parsons <dpars@hotmail.co.uk>
2021-12-15 22:03:50 +0900

    links

--- src/Components/News/NewsItem/NewsItem.js
+++ src/Components/News/NewsItem/NewsItem.js
@@ -6,6 +6,7 @@ function NewsItem( {itemData} ) {
         <div className={styles.newsItemArea}>
             <h2 className={styles.title}>{itemData.title}</h2>
             <p className={styles.info}>{itemData.info}</p>
+            <a className={styles.link} href={itemData.link} target="_blank" rel="noopener noreferrer">{itemData.linkText}</a>
             <p className={styles.date}>{itemData.date}</p>
         </div>
     )
--- src/Text/newsText.js
+++ src/Text/newsText.js
@@ -2,12 +2,16 @@ export const englishText = [
     {
         title: "Number One",
         info: "This is article one",
+        link: "https://9bordencoffee.com",
+        linkText: "Click here",
         date: "Nov 2021"
     },
 
     {
         title: "Number Two",
         info: "This is article two",
+        link: "https://9bordencoffee.com",
+        linkText: "Click here",
         date: "Nov 2021"
     }
 ]
@@ -16,12 +20,16 @@ export const japaneseText = [
     {
         title: "一番",
         info: "これは一番です",
+        link: "https://9bordencoffee.com",
+        linkText: "ここで見る",
         date: "2021年 11月"
     },
 
     {
         title: "二番",
         info: "これは二番です",
+        link: "https://9bordencoffee.com",
+        linkText: "ここで見る",
         date: "2021年　11月"
     },
 
@@ -34,12 +42,16 @@ export const englishText = [
     {
         title: "Number One",
         info: "This is article one",
+        link: "https://9bordencoffee.com",
+        linkText: "Click here",
         date: "Nov 2021"
     },
 
     {
         title: "Number Two",
         info: "This is article two",
+        link: "https://9bordencoffee.com",
+        linkText: "Click here",
         date: "Nov 2021"
     }
 ]
@@ -48,14 +60,19 @@ export const japaneseText = [
     {
         title: "一番",
         info: "これは一番です",
+        link: "https://9bordencoffee.com",
+        linkText: "ここで見る",
         date: "2021年 11月"
     },
 
     {
         title: "二番",
         info: "これは二番です",
+        link: "https://9bordencoffee.com",
+        linkText: "ここで見る",
         date: "2021年　11月"
-    }
+    },
+
 ]
 
 */
