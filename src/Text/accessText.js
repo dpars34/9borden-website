bffd41b8791b7e0faf2fc4ad2a57e48fa5b05347
Daniel Parsons <daniel.parsons@actbe.co.jp>
2022-03-12 13:05:44 +0900

    email added to access section

--- src/Text/accessText.js
+++ src/Text/accessText.js
@@ -2,9 +2,11 @@ export const accessText = {
     englishLocation: "3 Chome-7-15 Chikko, Minato Ward, Osaka, 552-0021",
     englishTransport: "2 minute walk from Osakako Station (Chuo Line)",
     englishPhone: "090-6605-9192",
+    englishEmail: "shop@9bordencoffee.com",
     japaneseLocation: "大阪市港区築港3丁目7−15 港振興ビル 111号室",
     japaneseTransport: "大阪メトロ中央線 大阪港駅から徒歩2分",
     japanesePhone: "090-6605-9192",
+    japaneseEmail: "shop@9bordencoffee.com",
 
     englishMonday: "7:30 - 18:00",
     englishTuesday: "7:30 - 18:00",
